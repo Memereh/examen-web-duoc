5b142b11ea510806616afaa27ab11ba8cd532103
Memereh <jeremy.vargas@outlook.com>
2022-07-08 15:54:06 -0400

    se agregan los templates webs

--- examen/app/views.py
+++ examen/app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'app/home.html')
+
+def about(request):
+    return render(request, 'app/sobrenosotros.html')
+
+def productos(request):
+    return render(request, 'app/productos.html')
